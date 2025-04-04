c74c2e78dbcb6ebebaacca79651953fb05e110db
Leonardo Sánchez Ferrer <lsanchez@encamina.com>
2025-04-04 13:54:26 +0200

    Añadido env.d.ts para ver si lee bien las variables

---
+++ Holocron.Web/env.d.ts
@@ -0,0 +1,10 @@
+﻿/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    VITE_BACKEND_BASE_URL: string;
+    // Add other environment variables here
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
