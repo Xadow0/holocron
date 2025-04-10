89d26a9b65b0cde53a3713578f8f5f7322b978b4
Leonardo Sánchez Ferrer <lsanchez@encamina.com>
2025-04-10 10:57:40 +0200

    Cambios para utilizar la URL correcta del back en el front

--- Holocron.Web/src/utils/api.ts
+++ Holocron.Web/src/utils/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 import type { Planet } from '../types/planets'
 
-//const API_BASE_URL = 'https://lsanwebapp01-ahdhasdffjczcehe.spaincentral-01.azurewebsites.net/api' // Backend URL
-const API_BASE_URL = 'https://localhost:7278/api'
+const API_BASE_URL = 'https://lsanwebapp01-ahdhasdffjczcehe.spaincentral-01.azurewebsites.net/api' // Backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
